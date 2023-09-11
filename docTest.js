75e94b87267ad8786e9a8e77606ab8ca7624315d
Teesitin <teesitin.russell@gmail.com>
2023-09-11 13:36:58 -0400

    Create docTest.js

---
+++ docTest.js
@@ -0,0 +1,20 @@
+/**
+ * Represents a book.
+ * @constructor Book - 
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+function Book(title, author) {
+}
