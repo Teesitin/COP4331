cd377353b31157538960836fa85628c7981eaee3
MarkMango21 <mark2asf@gmail.com>
2023-09-27 07:06:39 -0700

    Creation of the User and Contact classes

---
+++ User&Contact.php
@@ -0,0 +1,13 @@
+<?php
+class WUser {
+    // Class properties (variables)
+    public $id;
+    //public
+
+    // Class methods (functions)
+    /*public function myMethod() {
+        // Method code here
+    }*/
+}
+
+?>
