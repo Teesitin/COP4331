94450b21ee8c341b2eb179f6ec1679216a11a055
MarkMango21 <mark2asf@gmail.com>
2023-09-30 18:27:48 -0700

    Commented  on login.php file
    Made some little progress in the php file.

--- api/login.php
+++ api/login.php
@@ -27,7 +27,41 @@ if( $connection->connect_error )
         
             // Code inside this block will execute if the request method is POST
         }
-        
+        /*$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Sanitize and hash the password for comparison
+$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+// Query the database to retrieve user information
+$query = "SELECT id, username, password FROM users WHERE username = ?";
+$stmt = $connection->prepare($query);
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+// Check if a user with the provided username exists
+if ($result->num_rows === 1) {
+    $user = $result->fetch_assoc();
+
+    // Verify the provided password against the hashed password in the database
+    if (password_verify($password, $user['password'])) {
+        // Successful login; set session variables, redirect, or perform other actions
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        // Incorrect password
+        echo "Incorrect password. Please try again.";
+    }
+} else {
+    // User with the provided username does not exist
+    echo "User not found. Please check your username.";
+}
+// Close the database connection
+$connection->close();
+*/ 
         //control or optionn 2
         
 
