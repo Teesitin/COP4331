2894d3c4aee9339c6cd51972c1cfeba9160fc082
Nathan <nathanrodriguez240@gmail.com>
2023-10-02 12:47:04 -0400

    Created User class
    Changed the user db to be a class. Also added functions (still incomplete) to set and verify password.

--- User.php
+++ User.php
@@ -1,34 +1,59 @@
 <!-- Establishing Connection-->
 <?php
-// Change these fields if testing on local mysql server
-$servername = "localhost";
-$username = "contact_man";
-$password = "BxFj&Y4qd!Y6Mw";
-$database = "cm_db";
 
-// Create connection
-$db = new mysqli($servername, $username, $password, $database);
+class User
+{
+  // Change these fields if testing on local mysql server
+  private $servername = "localhost";
+  private $username = "contact_man";
+  private $password = "BxFj&Y4qd!Y6Mw";
+  private $database = "cm_db";
+  private $db;
+  
+  private function connect()
+  {
+    // Create connection
+      $db = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($db->connect_error) {
-  die("Connection failed: " . $db->connect_error);
+      // Check connection
+      if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+      }
+  }
+
+  function createTable()
+  {
+    connect();
+
+    // User table
+    $sql = "CREATE TABLE IF NOT EXISTS User(
+    ID INT(6) PRIMARY KEY,
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    userName VARCHAR(50),
+    profileImg VARCHAR(256),
+    dateCreated DATETIME,
+    dateLastLoggedIn DATETIME,
+    'password' VARCHAR(256)
+    )";
+  
+    if ($db->query($sql) === TRUE) {
+      echo "Table User created successfully";
+      } else {
+      echo "Error creating table: " . $db->error;
+      }
+  }
+
+  function set_password($string)
+  {
+    
+  }
+
+  function verify_password($string)
+  {
+
+  }
+
 }
 
-// User table
-$sql = "CREATE TABLE IF NOT EXISTS User(
-ID INT(6) PRIMARY KEY,
-firstName VARCHAR(50),
-lastName VARCHAR(50),
-userName VARCHAR(50),
-profileImg VARCHAR(256),
-dateCreated DATETIME,
-dateLastLoggedIn DATETIME,
-'password' VARCHAR(256)
-)";
-
-if ($db->query($sql) === TRUE) {
-	echo "Table User created successfully";
-  } else {
-	echo "Error creating table: " . $db->error;
-  }
 ?>
