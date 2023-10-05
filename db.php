a37bca38546e232e3686d9b6e7ee5f71c649a29a
Nathan <nathanrodriguez240@gmail.com>
2023-10-05 18:48:31 -0400

    Modified connection script
    Moved the script that connects to db into its own file to create connection only once. The file is called db.php and is included in the user_controller.php file

---
+++ db.php
@@ -0,0 +1,35 @@
+<?php
+
+	$servername = "localhost";
+	$username = "contact_man";
+	$password = "BxFj&Y4qd!Y6Mw";
+	$database = "cm_db";
+	$db;
+
+	// Create connection
+    $db = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($db->connect_error) {
+      die("Connection failed: " . $db->connect_error);
+    }
+
+    // User table
+    $sql = "CREATE TABLE IF NOT EXISTS User(
+    ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    userName VARCHAR(50),
+    profileImg VARCHAR(256),
+    dateCreated DATETIME,
+    dateLastLoggedIn DATETIME,
+    'password' VARCHAR(256)
+    )";
+  
+    if ($db->query($sql) === TRUE) {
+      echo "Table User created successfully";
+      } else {
+      echo "Error creating table: " . $db->error;
+      }
+
+?>
