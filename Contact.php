04f66f9d9d80b5af3a7cc71086e90a1098a6f693
Charity <90201079+CCRollins@users.noreply.github.com>
2023-10-06 09:32:52 -0400

    Updated Contact Model/Controller

--- Contact.php
+++ Contact.php
@@ -3,7 +3,6 @@
 
 class Contact
 {
-  // Change these fields if testing on local mysql server
   private $servername = "localhost";
   private $username = "contact_man";
   private $password = "BxFj&Y4qd!Y6Mw";
@@ -34,63 +33,6 @@ class Contact
       echo "Table Contact created successfully";
       } else {
       echo "Error creating table: " . $this->db->error;
-      }
-  }
-
-  // Create
-  public function createContact($firstName, $lastName, $mobilePhone, $homePhone, $email)
-  {
-      $firstName = $this->db->real_escape_string($firstName);
-      $lastName = $this->db->real_escape_string($lastName);
-      $mobilePhone = $this->db->real_escape_string($mobilePhone);
-      $homePhone = $this->db->real_escape_string($homePhone);
-      $email = $this->db->real_escape_string($email);
-
-      $sql = "INSERT INTO " . $this->table . " (firstName, lastName, mobilePhone, homePhone, email) VALUES ('$firstName', '$lastName', '$mobilePhone', '$homePhone', '$email')";
-
-      if ($this->db->query($sql) === TRUE) {
-          echo "Contact created successfully";
-      } else {
-          echo "Error creating contact: " . $this->db->error;
-      }
-  }
-
-  // Retrieve
-  public function retrieveContacts()
-  {
-
-  }
-
-  // Delete
-  public function deleteContact($ID)
-  {
-      $ID = $this->db->real_escape_string($ID);
-
-      $sql = "DELETE FROM " . $this->table . " WHERE ID = $ID";
-
-      if ($this->db->query($sql) === TRUE) {
-          echo "Contact deleted successfully";
-      } else {
-          echo "Error deleting contact: " . $this->db->error;
-      }
-  }
-
-  // Update
-  public function updateContact($ID, $firstName, $lastName, $mobilePhone, $homePhone, $email)
-  {
-      $ID = $this->db->real_escape_string($ID);
-      $firstName = $this->db->real_escape_string($firstName);
-      $lastName = $this->db->real_escape_string($firstName);
-      $mobilePhone = $this->db->real_escape_string($mobilePhone);
-      $homePhone = $this->db->real_escape_string($mobilePhone);
-      $email = $this->db->real_escape_string($mobilePhone);
-
-      $sql = "UPDATE " . $this->table . " SET firstName = '$firstName', lastName = '$lastName', mobilePhone = '$mobilePhone', homePhone = '$homePhone', email = '$email' WHERE ID = $ID";
-
-      if ($this->db->query($sql) === TRUE) {
-          echo "Contact updated successfully";
-      } else {
-          echo "Error updating contact: " . $this->db->error;
       }
   }
 }
