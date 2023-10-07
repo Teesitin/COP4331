d64bb088bfd4021aa801f3c2c3797b72346c0626
MarkMango21 <mark2asf@gmail.com>
2023-10-07 11:29:55 -0700

    login almost over

--- api/login.php
+++ api/login.php
@@ -3,22 +3,30 @@ include('request_handler.php');
 include('user_controller.php');
 include('User.php');
 include('db.php');
-//$inData=getRequestInfo();
+$inData=getRequestInfo();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
 
-    // Perform authentication here
-	//$connection = new $db;
- $SQL = "SELECT * FROM user WHERE userName = ?";
- $stmt = $db->prepare($SQL);
- $stmt->bind_param("s", $username);
- $stmt->execute();
- $result = $stmt->get_result();
- if ($result->num_rows > 0) {
-    $userData = $result->fetch_assoc();
-	if(verify_password($userData["password"])==1)
+ if ($inData) {
+    try
+	{
+      $user = read($inData->username);
+	  if($user)
+	  {
+		if(verify($user->password)==1){
+			return sendResultInfoAsJson($user);
+		}
+
+	  }
+	  else
+	  {
+		return 	"status" ; "error";
+	  }
+	}
+	catch( error)
+	{
+
+	}
+	/*if(verify_password($userData["password"])==1)
 	{
 
 	}
@@ -29,50 +37,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = create($user);
 	echo 'User created';
 
- }
+ }*/
 }
 
 
-
-
-
-/*$connection=new mysqli("hostname","user","password","database");
-if( $connection->connect_error )
-	{
-		 returnWithError( $connection->connect_error );
-	}
-    else{
-        //option one
-
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $stmt = $connection->prepare("SELECT ID,firstName,lastName FROM User WHERE Login=? AND Password =?");// this is a hold
-        $stmt->bind_param("ss", $inData["login"], $inData["password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
-        //If statement  where the user log in exist
-		if( $row = $result->fetch_assoc()  )
-		{
-			//returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'], $row['profilelmg'], $row['username'],$row['contacs']);
-		    $user = returnWithInfo( $row['Name'], $row['ID'], $row['leads'],$row['closed'], $row['sales'], $row['hours'],$row['phone'],$row['email']);
-
-            $userDataJSON= json_encode($user);
-			header("Location: menu.php?user=" . urlencode($userDataJSON));
-            exit();
-		}	
-
-		else
-		{
-			returnWithError("No Records Found");
-		}
-	}*/
-
-
-
-
-
-
-    
 
 
 ?>
